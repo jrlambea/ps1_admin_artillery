b122290f5729f9ded200c4c2cae510bdfad0b174
Newbie <jrlambea@users.noreply.github.com>
2016-06-30 17:36:23 +0200

    Create Verb-Nount.ps1

---
+++ Templates/Script/Verb-Nount.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+Describe the function here
+.DESCRIPTION
+Describe the function in more detail
+.EXAMPLE
+Give an example of how to use it
+.EXAMPLE
+Give another example of how to use it
+.PARAMETER computername
+The computer name to query. Just one.
+.PARAMETER logname
+The name of a file to write failed computer names to. Defaults to errors.txt.
+.INPUTS
+Inputs if any, otherwise state None
+.OUTPUTS
+Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log
+.LINK
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+#>
+
+#OPTIONAL: Set Error Action to Silently Continue
+#$ErrorActionPreference = "SilentlyContinue"
+
+[CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low')]
+param
+(
+  [Parameter(Mandatory=$True, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True, HelpMessage='What computer name would you like to target?')]
+  [Alias('host')]
+  [ValidateLength(3,30)]
+  [string$computername,
+  [string]$logname = 'errors.txt'
+)
+
+begin
+{
+  write-verbose "Deleting $logname"
+  del $logname -ErrorActionSilentlyContinue
+}
+
+process {
+
+  write-verbose "Beginning process loop"
+
+  foreach ($computer in $computername) {
+    Write-Verbose "Processing $computer"
+    if ($pscmdlet.ShouldProcess($computer)) { <# use $computer here #> }
+  }
+
+}
